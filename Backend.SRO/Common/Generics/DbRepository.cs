920209cb08a2bcef39ff8f0cb15ff4123cbb7540
Hiệp <hiepdev96@gmail.com>
2022-06-21 17:34:48 +0700

    frist commit

---
+++ Backend.SRO/Common/Generics/DbRepository.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Generics
+{
+    public class DbRepository<T, C> where T : class where C : DbContext
+    {
+        internal C Context;
+        internal DbSet<T> DbSet;
+
+        public DbRepository(C context)
+        {
+            Context = context;
+            DbSet = Context.Set<T>();
+            DbSet.AsNoTracking();
+        }
+
+        public IQueryable<T> All
+        {
+            get { return DbSet; }
+        }
+        public IQueryable<T> AllNoTracking
+        {
+            get { return DbSet.AsNoTracking(); }
+        }
+        public IQueryable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = DbSet;
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+            return query;
+        }
+        public T Find(object id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public T Finds(params object[] objs)
+        {
+            return DbSet.Find(objs);
+        }
+        public void Insert(T entityToInsert)
+        {
+            DbSet.Add(entityToInsert);
+        }
+
+        public void Insert(List<T> entityToInserts)
+        {
+            DbSet.AddRange(entityToInserts);
+        }
+
+        public IQueryable<E> GetAll<E>() where E : class
+        {
+            return Context.Set<E>();
+        }
+
+        public E Find<E>(object id) where E : class
+        {
+            return Context.Set<E>().Find(id);
+        }
+
+        public void Insert<E>(E entityToInsert) where E : class
+        {
+            Context.Set<E>().Add(entityToInsert);
+        }
+
+        public void Update<E>(E entityToUpdate) where E : class
+        {
+            Context.Set<E>().Attach(entityToUpdate);
+            Context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public void InsertAndSave(T entityToInsert)
+        {
+            Insert(entityToInsert);
+            Save();
+        }
+
+        public void InsertAndSave<E>(E entityToInsert) where E : class
+        {
+            Insert(entityToInsert);
+            Save();
+        }
+
+        public void Delete<E>(object id) where E : class
+        {
+            E entityToDelete = Context.Set<E>().Find(id);
+            this.Delete(entityToDelete);
+        }
+
+        public void Update(T entityToUpdate)
+        {
+            DbSet.Attach(entityToUpdate);
+            Context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public void UpdateAndSave(T entityToUpdate)
+        {
+            Update(entityToUpdate);
+            Save();
+        }
+
+
+        public void Delete(T entityToDelete)
+        {
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+            DbSet.Remove(entityToDelete);
+        }
+
+        public void DeleteAndSave(T entityToDelete)
+        {
+            Delete(entityToDelete);
+            Save();
+        }
+        public void Delete(object id)
+        {
+            T entityToDelete = DbSet.Find(id);
+            this.Delete(entityToDelete);
+        }
+        public void DeleteBy(Expression<Func<T, bool>> expression, params object[] parameters)
+        {
+            var query = this.All.Where(expression);
+            string sqlSelect = query.ToString();
+            int ind = sqlSelect.ToLower().IndexOf("from ");
+            string sqlDelete = "delete " + sqlSelect.Substring(ind);
+            ((IObjectContextAdapter)Context).ObjectContext.ExecuteStoreCommand(sqlDelete, parameters);
+        }
+        public void DeleteAll(List<T> entities)
+        {
+            foreach (T entity in entities)
+            {
+                this.Delete(entity);
+            }
+        }
+
+
+
+        public void Save()
+        {
+            try
+            {
+                Context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                string error = "";
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        error += string.Format("{0}:{1}; ", ve.PropertyName, ve.ErrorMessage);
+                    }
+
+                }
+                throw new Exception(error);
+            }
+        }
+        public void Dispose()
+        {
+            if (Context != null)
+            {
+                Context.Dispose();
+            }
+        }
+    }
+}
---
+++ Backend.SRO/Common/Generics/GenericService.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Generics
+{
+    public class GenericService<T, C> : IDisposable where T : class where C : DbContext
+    {
+
+        protected DbRepository<T, C> repository;
+
+        public C DbContext { get; set; }
+
+        //SSOReportEntities _rpcontext;
+        //public SSOReportEntities DbReportContext
+        //{
+        //    get
+        //    {
+        //        if (_rpcontext == null)
+        //        {
+        //            _rpcontext = new SSOReportEntities();
+        //        }
+        //        return _rpcontext;
+        //    }
+        //}
+
+        public GenericService(C context)
+        {
+            this.DbContext = context;
+            this.repository = new DbRepository<T, C>(context);
+        }
+
+        public virtual B GetService<B>()
+        {
+            return (B)typeof(B).GetConstructor(new Type[] { typeof(C) }).Invoke(new object[] { this.DbContext });
+        }
+
+        public virtual DbRepository<B, C> GetDbRepository<B>() where B : class
+        {
+            return new DbRepository<B, C>(this.DbContext);
+        }
+
+        public virtual IQueryable<T> All
+        {
+            get { return repository.All; }
+        }
+        public virtual IQueryable<T> AllNoTracking
+        {
+            get { return repository.AllNoTracking; }
+        }
+        public virtual IQueryable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
+        {
+            return repository.AllIncluding(includeProperties);
+        }
+
+        public virtual T Find(object id)
+        {
+            return repository.Find(id);
+        }
+        public virtual T Find(string name)
+        {
+            return repository.Find(name);
+        }
+        public virtual T CheckExists(object id)
+        {
+            T t = repository.Find(id);
+            if (t == null)
+            {
+                throw new Exception(
+                    $"Không tồn tại bản ghi có id = {id} trong database");
+            }
+            return t;
+        }
+
+        public virtual T Find(params object[] objs)
+        {
+            return repository.Finds(objs);
+        }
+        public virtual T Insert(T entityToInsert)
+        {
+            repository.Insert(entityToInsert);
+            return entityToInsert;
+        }
+
+        public virtual List<T> Insert(List<T> entityToInserts)
+        {
+            if (entityToInserts != null && entityToInserts.Any())
+            {
+                repository.Insert(entityToInserts);
+            }
+            return entityToInserts;
+        }
+
+        // insert new code
+
+        public virtual IQueryable<E> GetAll<E>() where E : class
+        {
+            return repository.GetAll<E>();
+        }
+
+        public virtual E Insert<E>(E entityToInsert) where E : class
+        {
+            repository.Insert<E>(entityToInsert);
+            return entityToInsert;
+        }
+
+        public virtual E Update<E>(E entityToUpdate) where E : class
+        {
+            repository.Update<E>(entityToUpdate);
+            return entityToUpdate;
+        }
+
+        public virtual void Delete<E>(object id) where E : class
+        {
+            repository.Delete<E>(id);
+
+        }
+
+        public virtual E Find<E>(object id) where E : class
+        {
+            return repository.Find<E>(id);
+        }
+        //the end
+
+        public virtual T Update(T entityToUpdate)
+        {
+            repository.Update(entityToUpdate);
+            return entityToUpdate;
+        }
+
+        public virtual T InsertAndSave(T entityToInsert)
+        {
+            repository.InsertAndSave(entityToInsert);
+            return entityToInsert;
+        }
+
+        public virtual E InsertAndSave<E>(E entityToInsert) where E : class
+        {
+            repository.InsertAndSave(entityToInsert);
+            return entityToInsert;
+        }
+
+        public virtual T UpdateAndSave(T entityToUpdate)
+        {
+            repository.UpdateAndSave(entityToUpdate);
+            return entityToUpdate;
+        }
+        public virtual void UpdateAndSave(ICollection<T> entityToUpdates)
+        {
+            foreach (var entityToUpdate in entityToUpdates)
+            {
+                repository.UpdateAndSave(entityToUpdate);
+            }
+        }
+
+        public virtual void Delete(object id)
+        {
+            repository.Delete(id);
+        }
+        public virtual void Delete(T entity)
+        {
+            repository.Delete(entity);
+        }
+
+        public virtual void DeleteAndSave(T entity)
+        {
+            repository.DeleteAndSave(entity);
+        }
+
+        public virtual void DeleteBy(Expression<Func<T, bool>> expression, params object[] parameters)
+        {
+            repository.DeleteBy(expression, parameters);
+        }
+        public virtual void DeleteAll(List<T> entities)
+        {
+            repository.DeleteAll(entities);
+        }
+        public virtual void Save()
+        {
+            repository.Save();
+        }
+        public string GetFilePath(string relativePath)
+        {
+            string assemblyFolder = Path.GetDirectoryName(
+              new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            return Path.Combine(assemblyFolder,
+                   relativePath);
+
+        }
+
+        //public void Dispose()
+        //{
+        //    if (_rpcontext != null)
+        //    {
+        //        _rpcontext.Dispose();
+        //    };
+        //}
+    }
+}
